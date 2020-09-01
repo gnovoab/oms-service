0a62e0a06ec458f5c44fe41cc52847907b27915f
gnovoab <Waze4Wo0!>
2020-09-01 23:55:23 +0100

    Changes logging configuration

--- src/main/java/com/ecommerce/oms/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/oms/repository/OrderRepository.java
@@ -5,6 +5,8 @@ package com.ecommerce.oms.repository;
 //Imports
 import com.ecommerce.oms.domain.model.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 }
--- src/main/java/com/ecommerce/oms/repository/PromotionRepository.java
+++ src/main/java/com/ecommerce/oms/repository/PromotionRepository.java
@@ -5,6 +5,8 @@ package com.ecommerce.oms.repository;
 //Imports
 import com.ecommerce.oms.domain.model.Promotion;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PromotionRepository extends CrudRepository<Promotion, Long> {
 }
