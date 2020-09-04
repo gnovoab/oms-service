bab812b9f56f269e71bae6f68c5bd86460415758
gnovoab <Waze4Wo0!>
2020-09-04 01:14:33 +0100

    Cleanup

--- src/test/java/com/ecommerce/oms/OmsApplicationTests.java
+++ src/test/java/com/ecommerce/oms/OmsApplicationTests.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Integration test
